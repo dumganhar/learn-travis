d41e8ecb48dbd32e67a65ef0eda12acdc93b10d5
James Chen <jianhua.chen@cocos2d-x.org>
2013-06-06 22:46:08 +0800

    Updating push.sh

--- travis-scripts/push_or_pr_to_robot.sh
+++ travis-scripts/push_or_pr_to_robot.sh
@@ -3,17 +3,20 @@
 # exit this script if any commmand fails
 set -e
 
+MY_NAME=CocosRobot2
+MY_PW=cc1234567890
+
+ELAPSEDSECS=`date +%s`
 #Set git user
 git config --global user.email yoursister@gmail.com
 git config --global user.name ${MY_NAME}
 
 git checkout -B master
-git remote add upstream https://${MY_NAME}:${MY_PW}@github.com/angeltown/learn-travis.git 2> /dev/null > /dev/null
+git remote add upstream https://${MY_NAME}:${MY_PW}@github.com/${MY_NAME}/learn-travis.git 2> /dev/null > /dev/null
 
 if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
 # It's a Pull Request, so we send a PR to repo of robot
-    curl --user "${MY_NAME}:${MY_PW}" --request POST --data "{ \"title\": \"Test :${TRAVIS_REPO_SLUG} PR${TRAVIS_PULL_REQUEST}\", \"body\": \"\", \"head\": \"${MY_NAME}:${TRAVIS_BRANCH}\", \"base\": \"${TRAVIS_BRANCH}\"}" https://api.github.com/repos/angeltown/learn-travis/pulls 2> /dev/null > /dev/null
-
+    git push -fq upstream ${TRAVIS_BRANCH}_${ELAPSEDSECS} 2> /dev/null
 else
 # It's a push or merge, so we push to repo of robot
     git push -fq upstream ${TRAVIS_BRANCH} 2> /dev/null
