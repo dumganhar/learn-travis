3ad6bca53788687eb617db6a9693a9c0e7d42686
James Chen <jianhua.chen@cocos2d-x.org>
2013-06-06 22:07:43 +0800

    Adding run-script.sh

---
+++ travis-scripts/run-script.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# exit this script if any commmand fails
+set -e
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+PROJECT_ROOT="$DIR"/..
+
+
+if [ $TEST_ON_MAC"aaa" = "YES"aaa ];then
+    ./push_or_pr_to_robot.sh
+else
+    cd $PROJECT_ROOT
+    $CXX helloworld.cpp -o helloworld
+    ./helloworld
+fi
+
